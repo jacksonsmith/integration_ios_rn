2379c9366eee2f23a06e657a9d3a86f9edef869f
Jackson Matias <jsmatias@thoughtworks.com>
2020-07-27 16:38:23 -0300

    Initial Setup

---
+++ Module Comunication/Module Comunication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Module Comunication
+//
+//  Created by Jackson Matias on 15/07/20.
+//  Copyright © 2020 jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        label.text = "Change Text"
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Module Comunication/Pods/Target Support Files/Pods-Module Comunication/Pods-Module Comunication-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Module_Comunication : NSObject
+@end
+@implementation PodsDummy_Pods_Module_Comunication
+@end
