1bac6b4d559c6224352373cfced70d4d36617936
Jackson Matias <jsmatias@thoughtworks.com>
2020-07-27 21:12:24 -0300

    Init 3

---
+++ index.ios.js
@@ -0,0 +1,37 @@
+'use strict';
+// 1
+import React from 'react';
+import ReactNative, {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+// 2
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+  },
+  welcome: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
+
+// 3
+class AddRatingApp extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>We're live from React Native!!!</Text>
+      </View>
+    )
+  }
+}
+
+// 4
+AppRegistry.registerComponent('AddRatingApp', () => AddRatingApp);
