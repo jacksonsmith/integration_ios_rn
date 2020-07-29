5fcbe6462d7c98572c540a9f30bad3f8281c2c85
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-07-29 09:40:14 -0300

    Enable multidex on the app

--- android/app/src/main/java/com/sellingcartapp/MainApplication.java
+++ android/app/src/main/java/com/sellingcartapp/MainApplication.java
@@ -2,6 +2,9 @@ package com.sellingcartapp;
 
 import android.app.Application;
 import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -80,6 +83,9 @@ public class MainApplication extends Application implements ReactApplication {
         }
     }
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
-  }
 }
