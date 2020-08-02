fa2323a78f0458a481fe4c020d2b34f58b44e97b
Lucas Conceição <lucas.conceicao@thoughtworks.com>
2020-08-02 12:07:54 -0300

    Add Android configuration to display toast

---
+++ android/app/src/main/java/com/sellingcartapp/ToastModule.kt
@@ -0,0 +1,18 @@
+package com.sellingcartapp
+
+import android.widget.Toast
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class ToastModule(
+        private val reactContext: ReactApplicationContext
+) : ReactContextBaseJavaModule() {
+
+    override fun getName(): String = "ToastModule"
+
+    @ReactMethod
+    fun show(text: String) {
+        Toast.makeText(reactContext, "text", Toast.LENGTH_LONG).show()
+    }
+}
